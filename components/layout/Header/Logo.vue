80b1040351d46facd414aee7325cbe627eb8b5ac
parakeet <masa.loves.it@gmail.com>
2026-01-12 16:21:04 +0900

    fix: top page url　

--- components/layout/Header/Logo.vue
+++ components/layout/Header/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtLink v-if="logo?.light && logo?.dark" to="/substances" class="flex">
+    <NuxtLink v-if="logo?.light && logo?.dark" to="https://www.truth-light.jp/" class="flex">
       <NuxtImg :src="logo.dark" class="relative top-[0.08rem] h-7 dark:block" />
       <span v-if="showTitle && title" class="ml-3 self-center text-2xl text-teal-400 font-bold drop-shadow-xl drop-shadow-teal-400"
       style="font-family: 'Avenir', 'Avenir Next', 'Segoe UI', 'Helvetica Neue', 'Arial', 'sans-serif';">
