0b53c273bb7d72bbf516ab9cb6f8cc0e55c91fce
psy-yumekawa <psy.yumekawa@proton.me>
2026-06-08 17:13:40 +0900

    add: GA tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,20 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'ja',
       },
+      script: [
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-2W5X248N5S',
+          async: true
+        },
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2W5X248N5S');
+          `
+        }
+      ],
     },
   },
   compatibilityDate: '2025-07-15',
