dfde7fe77034a15088b3644ac0b54f0b3ebac70e
psy-yumekawa <psy.yumekawa@proton.me>
2026-07-11 11:45:45 +0900

    chore: add version information to package.json

--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,7 @@ export default defineAppConfig({
       topPageName: 'Truth Light | 薬物依存のリカバリー＆ハームリダクション',
       topPageDescription: 'Truth Lightは「日本版Erowid」を目指し、薬物に対する正しい情報と依存症からの回復の道を提供します。',
       url: 'https://www.truth-light.jp/',
+      version: "1.0.1",
       ogImage: '/og-default.png'
     },
     header: {
