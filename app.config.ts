4fd7ae2aa62a15ec0ed3e44090a3a3550915e13f
psy-yumekawa <psy.yumekawa@proton.me>
2026-07-18 09:47:55 +0900

    chore: update version

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default defineAppConfig({
       topPageName: 'Truth Light | 薬物依存のリカバリー＆ハームリダクション',
       topPageDescription: 'Truth Lightは「日本版Erowid」を目指し、薬物に対する正しい情報と依存症からの回復の道を提供します。',
       url: 'https://www.truth-light.jp/',
-      version: "1.0.1",
+      version: "1.1.1",
       ogImage: '/ogp/default/ogp-default.png'
     },
     header: {
